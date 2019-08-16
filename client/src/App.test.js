31fe1c52f1f413ec39d1bf80a2bca5bd9f8a7b74
Alan S <alanstill@gmail.com>
2019-08-16 11:05:34 +0100

    inital commit

---
+++ client/src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+// commented out below as this will try and call the Fib.js
+// will tries to connect to the express server
+// in real world this would get mocked out
+
+  // const div = document.createElement('div');
+  // ReactDOM.render(<App />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
